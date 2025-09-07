b12cb5e0e25d5025c530b3ec9b6e2f08701c12f6
chaelin <chaeee0123@gmail.com>
2025-09-08 03:08:07 +0900

    :recycle:Refactor: print라인 지우기

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,10 +1,8 @@
 def solution(nums):
     pick = len(nums) / int(2)
-    print("pick:", pick)
     
     set_nums = set(nums)
     list_nums = list(set_nums)
-    print(list_nums)
     
     if len(list_nums) <= pick:
         return len(list_nums)
