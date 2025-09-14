940e1bbd0ff0cc1e6a1417033759e7347f4a51f0
chaelin <chaeee0123@gmail.com>
2025-09-15 02:08:20 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 9.02 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    slice = []
+    for i in commands:
+        slice = array[i[0]-1:i[1]]
+        slice.sort()
+        answer.append(slice[i[2] - 1])
+    return answer
