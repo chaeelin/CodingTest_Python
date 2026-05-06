3b94c2d2dcae6cccc166fe443caf1440d91689af
chaelin <chaeee0123@gmail.com>
2026-05-06 14:32:46 +0900

    [level 1] Title: 모의고사, Time: 4.79 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,26 +1,26 @@
 def solution(answers):
-    answer = []
-    a = b = c = 0
-    
     first = [1,2,3,4,5]
     second = [2,1,2,3,2,4,2,5]
     third = [3,3,1,1,2,2,4,4,5,5]
+    first_count = 0 
+    second_count = 0 
+    third_count = 0 
     
-    for i in range(len(answers)):
-        if answers[i] == first[i % len(first)]:
-            a += 1
-        if answers[i] == second[i % len(second)]:
-            b += 1
-        if answers[i] == third[i % len(third)]:
-            c += 1
-
-    if max(a,b,c) == a:
+    for i,answer in enumerate(answers):
+        if answer == first[i%len(first)]:
+            first_count += 1
+        if answer == second[i%len(second)]:
+            second_count += 1
+        if answer == third[i%len(third)]:
+            third_count += 1
+    
+    answer = []
+    if first_count == max(first_count,second_count,third_count):
         answer.append(1)
-        
-    if max(a,b,c) == b:
+    if second_count == max(first_count,second_count,third_count):
         answer.append(2)
-        
-    if max(a,b,c) == c:
+    if third_count == max(first_count,second_count,third_count):
         answer.append(3)
-
+    
+    answer.sort()    
     return answer
