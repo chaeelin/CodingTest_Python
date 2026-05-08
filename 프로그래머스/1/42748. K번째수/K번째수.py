3f56719e09d9e099c0697a65b85a0a66f9a7fd08
chaelin <chaeee0123@gmail.com>
2026-05-08 13:28:37 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,12 +1,17 @@
 def solution(array, commands):
     answer = []
-    list = []
+    i = 0
+    j = 0
+    j = 0
     
-    for i in range(len(commands)):
-        start = commands[i][0]
-        finish = commands[i][1]
-        cut = commands[i][2]
-        list = sorted(array[start-1:finish])
-        answer.append(list[cut-1])
-        
+    for num in range(len(commands)):
+        i = commands[num][0]
+        j = commands[num][1]
+        k = commands[num][2]
+        a = array[i-1:j]
+        a = sorted(a)
+        a = a[k-1]
+        answer.append(a)
+        i = 0
+        k = 0
     return answer
