df9d53601c180a1c5c67536042d56cdc691f4f71
chaelin <chaeee0123@gmail.com>
2026-07-13 17:12:06 +0900

    [level 1] Title: 폰켓몬, Time: 0.68 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,16 +1,9 @@
 def solution(nums):
-    answer = 0
-    pick = int(len(nums)/2)
-    count = 0
+    num = len(nums) / 2
+    
     nums = set(nums)
     
-    if len(nums) < pick:
-        answer = int(len(nums))
+    if num > len(nums):
+        return len(nums)
     else:
-        for i in range(1, len(nums)+1):
-            count += 1
-            
-            if count == pick:
-                answer = pick
-
-    return answer
+        return num
