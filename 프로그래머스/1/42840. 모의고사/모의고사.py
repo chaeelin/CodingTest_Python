3f1a80bc7c1906041d3d0cc22360d38816480b4b
chaelin <chaeee0123@gmail.com>
2026-07-20 14:48:51 +0900

    [level 1] Title: 모의고사, Time: 3.17 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,26 +1,20 @@
 def solution(answers):
-    first = [1,2,3,4,5]
-    second = [2,1,2,3,2,4,2,5]
-    third = [3,3,1,1,2,2,4,4,5,5]
-    first_count = 0 
-    second_count = 0 
-    third_count = 0 
-    
-    for i,answer in enumerate(answers):
-        if answer == first[i%len(first)]:
-            first_count += 1
-        if answer == second[i%len(second)]:
-            second_count += 1
-        if answer == third[i%len(third)]:
-            third_count += 1
-    
     answer = []
-    if first_count == max(first_count,second_count,third_count):
-        answer.append(1)
-    if second_count == max(first_count,second_count,third_count):
-        answer.append(2)
-    if third_count == max(first_count,second_count,third_count):
-        answer.append(3)
+    patterns = [
+        [1,2,3,4,5],
+        [2,1,2,3,2,4,2,5],
+        [3,3,1,1,2,2,4,4,5,5]
+    ]
+    scores = [0,0,0]
     
-    answer.sort()    
+    for i,ans in enumerate(answers):
+        for student, pattern in enumerate(patterns):
+            if ans == pattern[i%len(pattern)]:
+                scores[student] += 1
+    
+    max_score = max(scores)
+    
+    for i in range(len(scores)):
+        if max_score == scores[i]:
+            answer.append(i+1)
     return answer
