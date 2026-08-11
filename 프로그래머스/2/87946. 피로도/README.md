e4f1835c86f315c8df01bf3970f32047faa7efc9
chaelin <chaeee0123@gmail.com>
2026-08-11 14:52:18 +0900

    [level 2] Title: 피로도, Time: 19.47 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 11.8 MB, 시간: 14.90 ms
+메모리: 11.7 MB, 시간: 19.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 26일 17:52:30
+2026년 08월 11일 14:52:14
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,17 +1,24 @@
-from itertools import permutations
-
 def solution(k, dungeons):
-    answer = 0
+    answer = -1
+    visited = [False] * len(dungeons)
     result = []
-    kk = 0
     
-    for perm in permutations(dungeons):
-        kk = k
-        answer = 0
-        for dungeon in perm:
-            if (kk >= dungeon[0]):
-                kk = kk - dungeon[1]
-                answer += 1
-        result.append(answer)
+    def dfs(number, count):
+        nonlocal answer
         
-    return max(result)
+        answer = max(answer, count)
+        
+        for i in range(len(dungeons)):
+            if not visited[i] and number >= dungeons[i][0]:
+                visited[i] = True
+            
+                numbers = number - dungeons[i][1]
+            
+                dfs(numbers, count + 1)
+                
+                result.append(count)
+                visited[i] = False
+            
+    dfs(k,0)
+    
+    return answer
