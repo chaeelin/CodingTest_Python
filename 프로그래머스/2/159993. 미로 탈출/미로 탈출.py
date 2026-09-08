27f40fb763f2d4599e15f841737236369824cafc
chaelin <chaeee0123@gmail.com>
2026-09-08 16:45:29 +0900

    [level 2] Title: 미로 탈출, Time: 5.68 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -4,43 +4,43 @@ def solution(maps):
     n = len(maps)
     m = len(maps[0])
     
-    def bfs(start, target):
-        q = deque()
-        q.append((start[0],start[1], 0))
+    def bfs(sx,sy,target):        
         visited = [[False] * m for _ in range(n)]
+        distance = [[0] * m for _ in range(n)]
         
-        visited[start[0]][start[1]] = True
-        
-        dx = [1, -1, 0, 0]
-        dy = [0, 0, -1, 1]
-        
-        while q:
-            x, y, dist = q.popleft()
+        queue = deque([(sx,sy)])
+        visited[sx][sy] = True
+        distance[sx][sy] = 0
+    
+        while queue:
+            x,y = queue.popleft()
             
-            for i in range(4):
-                nx, ny = x + dx[i], y + dy[i]
+            for dx,dy in ((-1,0), (1,0), (0,1), (0,-1)):
+                nx = x + dx
+                ny = y + dy
                 
-                if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and maps[nx][ny] != "X":
+                if 0 <= nx < n and 0 <= ny < m:
+                    if not visited[nx][ny] and maps[nx][ny] != "X":
+                        visited[nx][ny] = True
+                        distance[nx][ny] = distance[x][y] + 1
                     
-                    visited[nx][ny] = True
-                    q.append((nx,ny, dist + 1))
+                        if maps[nx][ny] == target:
+                            return distance[nx][ny]
                     
-                    if maps[nx][ny] == target:
-                        return dist + 1
-                    
+                        queue.append((nx,ny))
         return -1
+    
+    left = -1
+    right = -1
     
     for i in range(n):
         for j in range(m):
             if maps[i][j] == "S":
-                start = (i, j)
-            elif maps[i][j] == "L":
-                lever = (i,j)
+                left = bfs(i,j,"L")
+            if maps[i][j] == "L":
+                right = bfs(i,j,"E")
 
-    a = bfs(start,'L')
-    b = bfs(lever, 'E')
-    
-    if a == -1 or b == -1:
+    if left == -1 or right == -1:
         return -1
     
-    return a + b
+    return left + right
